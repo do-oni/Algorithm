b3f4a67621d996d57eaaea116c046d33db0bc945
do-oni <dxxneo@gmail.com>
2021-08-22 20:47:09 +0900

    Update 전화번호 목록.java

--- Study/5.Hash/Programmers/전화번호 목록.java
+++ Study/5.Hash/Programmers/전화번호 목록.java
@@ -1,1 +1,19 @@
+import java.util.HashMap;
 
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String, String> hm = new HashMap<>();
+		for(String num : phone_book) {
+			hm.put(num, num);
+		}
+		for (String num : phone_book) {
+			for(int i=0; i<num.length(); i++) {
+				if(hm.containsKey(num.substring(0,i))) {
+					return false;
+				}
+			}	
+		}
+        return answer;
+    }
+}
