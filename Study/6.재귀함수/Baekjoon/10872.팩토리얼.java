219cb7ff5844d4777a55dbba82e8e2f8ae531cf8
do-oni <dxxneo@gmail.com>
2021-09-03 00:01:57 +0900

    Update 10872.팩토리얼.java

--- Study/6.재귀함수/Baekjoon/10872.팩토리얼.java
+++ Study/6.재귀함수/Baekjoon/10872.팩토리얼.java
@@ -1,1 +1,39 @@
-f
+//재귀
+import java.io.*;
+
+public class Main {
+    
+    public static int factorial(int n) {
+        if(n <= 1) {
+            return 1;
+        }else {
+            return n * factorial(n-1);
+            }
+        }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = factorial(n);
+        System.out.println(answer);
+    }
+}​
+
+
+//반복문
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = 1;
+        
+        while(n != 0) {
+            answer = answer * n;
+            n--;
+        }
+        System.out.println(answer);
+    }
+}
