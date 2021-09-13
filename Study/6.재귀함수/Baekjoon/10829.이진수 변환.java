a1e77e297c8dec6fa078f50c96f5578d0094d20e
do-oni <dxxneo@gmail.com>
2021-09-13 22:58:11 +0900

    Update 10829.이진수 변환.java

--- Study/6.재귀함수/Baekjoon/10829.이진수 변환.java
+++ Study/6.재귀함수/Baekjoon/10829.이진수 변환.java
@@ -1,1 +1,28 @@
+package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+	public class B10829 {
+		
+	    static String s = "";
+	    public static void convertBinary(long n) {
+	        if(n <= 1) {
+	            s += String.valueOf(n);
+	        }else {
+	            s += String.valueOf(n % 2);
+	            n = n / 2;
+	            convertBinary(n);
+	        }
+	    }
+	    
+	    public static void main(String[] args) throws IOException {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+	        long n = Long.parseLong(br.readLine());
+	        convertBinary(n);
+	        StringBuilder sb = new StringBuilder(s);
+	        s = sb.reverse().toString();
+	        System.out.println(s);
+	    }
+	}
