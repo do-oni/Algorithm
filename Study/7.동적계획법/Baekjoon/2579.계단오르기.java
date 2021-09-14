50a73d3c36feee1015ec3e0b7588424afe254785
do-oni <dxxneo@gmail.com>
2021-09-14 23:58:59 +0900

    Update 2579.계단오르기.java

--- Study/7.동적계획법/Baekjoon/2579.계단오르기.java
+++ Study/7.동적계획법/Baekjoon/2579.계단오르기.java
@@ -1,1 +1,32 @@
+import java.io.*;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int num = Integer.parseInt(br.readLine());
+ 
+		int[] answer = new int[num + 1];
+		int[] step = new int[num + 1];
+ 
+ 
+		for (int i = 1; i <= num; i++) {
+			step[i] = Integer.parseInt(br.readLine());
+		}
 
+		answer[1] = step[1];
+		
+		// N 이 1이 입력될 수도 있기 때문에 예외처리를 해줄 필요가 있다.
+		if (num >= 2) {
+			answer[2] = step[1] + step[2];
+		}
+ 
+		for (int i = 3; i <= num; i++) {
+			answer[i] = Math.max(answer[i - 2] , answer[i - 3] + step[i - 1]) + step[i];
+		}
+ 
+		System.out.println(answer[num]);
+ 
+	}
+}
