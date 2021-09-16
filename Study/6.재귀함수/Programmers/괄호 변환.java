62e288326046e646b5f75240b4950afe71201075
do-oni <dxxneo@gmail.com>
2021-09-16 23:24:37 +0900

    Update 괄호 변환.java

--- Study/6.재귀함수/Programmers/괄호 변환.java
+++ Study/6.재귀함수/Programmers/괄호 변환.java
@@ -6,7 +6,7 @@ class Solution {
     boolean isCorrect(String str){
         boolean ret = true;
 
-        int left =0, right = 0;//오른쪽 왼쪽 카운팅
+        int left =0, right = 0;
         Stack<Character> mystack = new Stack<>();
 
         for(int i =0; i<str.length(); ++i){
@@ -21,13 +21,12 @@ class Solution {
                     mystack.pop();
                 }
             }
-            if(left==right){//균형잡힌 괄호문자열인경우. (최초로)
-                //u,v를 분리하는부분
-                pos =i+1;//v의 시작위치이자, u의 length로 사용
+            if(left==right){  //u,v를 분리
+                pos=i+1;  //v의 시작위치이자, u의 length로 사용
                 return ret;
             }
         }
-        return true;//여기까지 올일은없음.
+        return true;  //여기까지 올일은없음.
     }
 
     public String solution(String p) {
@@ -37,11 +36,10 @@ class Solution {
         String u = p.substring(0, pos);
         String v = p.substring(pos, p.length());
 
-        if(correct){//3번인경우(올바른괄호문자열이라면)
-            return u + solution(v);//재귀적으로 solution메소드 호출
+        if(correct){  //3번인경우(올바른괄호문자열이라면)
+            return u + solution(v);  //재귀적으로 solution메소드 호출
         }
-        //else
-        //올바른문자열이 아니면.
+        
         String answer = "(" + solution(v) + ")";
         for(int i=1; i<u.length()-1; i++){//4-4
             if(u.charAt(i) == '(')
@@ -50,6 +48,6 @@ class Solution {
                 answer+="(";
         }
 
-        return answer;//4-5
+        return answer;  //4-5
     }
-}
+}﻿​
