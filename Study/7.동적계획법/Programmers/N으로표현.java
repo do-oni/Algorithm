d5b94c04e84d69c51ee9691c1c09059c73f8a302
do-oni <dxxneo@gmail.com>
2021-09-16 22:48:01 +0900

    Update N으로표현.java

--- Study/7.동적계획법/Programmers/N으로표현.java
+++ Study/7.동적계획법/Programmers/N으로표현.java
@@ -4,38 +4,38 @@ import java.util.List;
 import java.util.Set;
 
 class Solution { 
-		public int solution(int N, int number) { 
-			int answer = -1; 
-			List<Set<Integer>> list = new ArrayList<>(); 
-			for (int i = 0 ; i < 8; i++) { 
-				list.add(new HashSet<>()); 
-				} list.get(0).add(N); 
-				 
-				for (int i = 0; i < 8; i++) { 
-					list.get(i).add(Integer.parseInt(stringRepeat(N, i + 1))); 
-					for (int j = 0; j < i; j++) { 
-						for (int case1 : list.get(j)) { 
-							for (int case2 : list.get(i - 1 - j)) { 
-								list.get(i).add(case1 + case2); 
-								list.get(i).add(case1 - case2); 
-								list.get(i).add(case1 * case2); 
-								if (case2 != 0) { 
-									list.get(i).add(case1 / case2); 
-									} 
+	public int solution(int N, int number) { 
+		int answer = -1; 
+		List<Set<Integer>> list = new ArrayList<>(); 
+		for (int i = 0 ; i < 8; i++) { 
+			list.add(new HashSet<>()); 
+			} list.get(0).add(N); 
+
+			for (int i = 0; i < 8; i++) { 
+				list.get(i).add(Integer.parseInt(stringRepeat(N, i + 1))); 
+				for (int j = 0; j < i; j++) { 
+					for (int case1 : list.get(j)) { 
+						for (int case2 : list.get(i - 1 - j)) { 
+							list.get(i).add(case1 + case2); 
+							list.get(i).add(case1 - case2); 
+							list.get(i).add(case1 * case2); 
+							if (case2 != 0) { 
+								list.get(i).add(case1 / case2); 
 								} 
 							} 
 						} 
-					if (list.get(i).contains(number)) { 
-						answer = i + 1; 
-						break; 
-						} 
 					} 
-				return answer; 
+				if (list.get(i).contains(number)) { 
+					answer = i + 1; 
+					break; 
+					} 
 				} 
-		private String stringRepeat(int N, int times) { 
-			StringBuilder answer = new StringBuilder(); 
-			String str = String.valueOf(N); 
-			answer.append(str.repeat(Math.max(0, times))); 
-			return answer.toString(); 
+			return answer; 
 			} 
-		}
+	private String stringRepeat(int N, int times) { 
+		StringBuilder answer = new StringBuilder(); 
+		String str = String.valueOf(N); 
+		answer.append(str.repeat(Math.max(0, times))); 
+		return answer.toString(); 
+		} 
+	}
