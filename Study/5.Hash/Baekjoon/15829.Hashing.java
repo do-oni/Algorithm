b07e1fcf9a365935538a72e948be0c885198b5a9
do-oni <dxxneo@gmail.com>
2021-09-19 22:41:07 +0900

    Update 15829.Hashing.java

--- Study/5.Hash/Baekjoon/15829.Hashing.java
+++ Study/5.Hash/Baekjoon/15829.Hashing.java
@@ -18,22 +18,20 @@ public class Hashing{
 //        System.out.println(answer);
 //    }
 	
+   static final int n = 31;
+   static final int mod = 1234567891;
 
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int len = scan.nextInt();
+      String s = scan.next();
+      long pow = 1;
+      long  answer = 0; 
 
-	   static final int n = 31;
-	   static final int mod = 1234567891;
-	   
-	   public static void main(String[] args) {
-	      Scanner scan = new Scanner(System.in);
-	      int len = scan.nextInt();
-	      String s = scan.next();
-	      long pow = 1;
-	      long  answer = 0; 
-	      
-	      for(int i = 0; i < len; i++) {
-	         answer += (s.charAt(i) - 'a' + 1) * pow ;
-	         pow = (pow *= n) % mod;
-	      }
-	      System.out.println(answer % mod);
-	   }
-	}
+      for(int i = 0; i < len; i++) {
+	 answer += (s.charAt(i) - 'a' + 1) * pow ;
+	 pow = (pow *= n) % mod;
+      }
+      System.out.println(answer % mod);
+   }
+}
