ad8f036a0ede8f557e84272b2343b8d90ab89645
do-oni <dxxneo@gmail.com>
2021-09-28 00:28:18 +0900

    Update 체육복.java

--- Study/9.탐욕법/Programmers/체육복.java
+++ Study/9.탐욕법/Programmers/체육복.java
@@ -5,7 +5,7 @@ public class Solution {
 	public static int solution(int n, int[] lost, int[] reserve) {
     	int answer = n - lost.length;  //체육복이 있는 학생 수
 
-		Arrays.sort(reserve);
+	Arrays.sort(reserve);
         Arrays.sort(lost);
         
         //여분 체육복이 도난 당했을 경우
@@ -18,17 +18,17 @@ public class Solution {
 				}
 			}
 		}
-		//여분 체육복 빌리기
-		for(int i=0 ; i<reserve.length ; i++) {
-			for (int j=0 ; j<lost.length; j++) {
-				if (Math.abs(reserve[i] - lost[j]) == 1) {
-					reserve[i] = -1;
-					lost[j] = -1;
-					answer++;
-					break;
-				}
+	//여분 체육복 빌리기
+	for(int i=0 ; i<reserve.length ; i++) {
+		for (int j=0 ; j<lost.length; j++) {
+			if (Math.abs(reserve[i] - lost[j]) == 1) {
+				reserve[i] = -1;
+				lost[j] = -1;
+				answer++;
+				break;
 			}
-		} 
+		}
+	} 
         return answer;
-	}
+    }
 }
