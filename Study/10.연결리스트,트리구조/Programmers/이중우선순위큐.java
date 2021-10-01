8187cf4ef4a9d2b3f56874334d48bb6034bd6f3d
do-oni <dxxneo@gmail.com>
2021-10-02 02:33:34 +0900

    Create 이중우선순위큐.java

---
+++ Study/10.연결리스트,트리구조/Programmers/이중우선순위큐.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Solution {
+
+    public int[] solution(String[] oerations) {
+        int[] answer = new int[2];
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();  //오름차순
+        PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());  //내림차순
+        
+        for (String o : oerations) {
+            
+            if (o.substring(0,1).equals("I")) {
+                int value = Integer.valueOf(o.substring(2));     
+                pq.offer(value);
+                maxPq.offer(value);          
+            }          
+           
+            else if (!pq.isEmpty()) {
+                //최대값 삭제
+                if (o.equals("D 1")) {
+                    int value = maxPq.poll();
+                    pq.remove(value);
+                }
+                //최소값 삭제
+                else if (o.equals("D -1")) {
+                    int value = pq.poll();
+                    maxPq.remove(value);       
+                }
+            }            
+        }
+
+        if (!pq.isEmpty()) {
+            answer[0] = maxPq.peek();
+            answer[1] = pq.peek();
+        }
+
+        return answer;
+	    }
+	}﻿​
