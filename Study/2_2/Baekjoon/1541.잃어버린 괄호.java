4456d4cd3a8aaece9320d783e3172126912e7c15
do-oni <dxxneo@gmail.com>
2021-11-02 23:23:32 +0900

    Update 1541.잃어버린 괄호.java

--- Study/2_2/Baekjoon/1541.잃어버린 괄호.java
+++ Study/2_2/Baekjoon/1541.잃어버린 괄호.java
@@ -5,20 +5,20 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int sum = Integer.MAX_VALUE;
-		StringTokenizer subtract = new StringTokenizer(br.readLine(), "-");
-		
-		while(subtract.hasMoreTokens()) {
-			int temp = 0;
-			StringTokenizer add = new StringTokenizer(subtract.nextToken(), "+");
-			
-			while(add.hasMoreTokens()) temp += Integer.parseInt(add.nextToken());
-			
-			if(sum == Integer.MAX_VALUE) sum = temp;
-			else sum -= temp;
-		}
-		System.out.println(sum);
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	int sum = Integer.MAX_VALUE;
+	StringTokenizer subtract = new StringTokenizer(br.readLine(), "-");
+
+	while(subtract.hasMoreTokens()) {
+		int temp = 0;
+		StringTokenizer add = new StringTokenizer(subtract.nextToken(), "+");
+
+		while(add.hasMoreTokens()) temp += Integer.parseInt(add.nextToken());
+
+		if(sum == Integer.MAX_VALUE) sum = temp;
+		else sum -= temp;
 	}
+	System.out.println(sum);
+    }
 }
