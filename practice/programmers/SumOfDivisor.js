0e94b7b09760cb8bce2baf34c0ee4240a22eee1b
do-oni <dooniseo@gmail.com>
2022-02-13 04:27:09 +0900

    Update SumOfDivisor.js

--- practice/programmers/SumOfDivisor.js
+++ practice/programmers/SumOfDivisor.js
@@ -1,8 +1,8 @@
 function solution(n) {
-    var answer = 0;
+    let answer = 0;
     
-    for(let i=1; i<=n; i++) {
-        if(n % i === 0 && n <= 3000) {
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0 && n <= 3000) {
             answer += i;
         }
     }
