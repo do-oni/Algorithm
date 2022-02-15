3c8e51c08345064ca85fc2d4f5db0f05a20a4d3b
do-oni <dooniseo@gmail.com>
2022-02-15 18:24:01 +0900

    [Codility] Frog Jmp

--- practice/codility/FrogJmp.java
+++ practice/codility/FrogJmp.java
@@ -4,7 +4,7 @@ public class FrogJmp {
 
     public int solution(int X, int Y, int D) {
         // write your code in Java SE 8
-        double cnt = (Y - X) * 1.0 / D;
+        double cnt = (Y - X) * 1.0 / D; // 자동 형변환
 
         return (int) Math.ceil(cnt);
     }
