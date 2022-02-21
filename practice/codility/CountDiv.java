d04cbe21f173cee16bfbd7512ab7490b78f43d1c
do-oni <dooniseo@gmail.com>
2022-02-21 23:49:21 +0900

    Update CountDiv.java

--- practice/codility/CountDiv.java
+++ practice/codility/CountDiv.java
@@ -3,6 +3,7 @@ package main.java.algorithm.practice.codility;
 public class CountDiv {
 
     // Performance 50
+    // O(B-A)
     public int solution1(int A, int B, int K) {
         // write your code in Java SE 8
         int cnt = 0;
@@ -18,6 +19,7 @@ public class CountDiv {
     }
 
     // Performance 100
+    // O(1)
     public int solution2(int A, int B, int K) {
         // write your code in Java SE 8
         int cnt = (B / K) - (A / K);
