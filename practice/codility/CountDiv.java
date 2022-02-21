70ef8833186572ebf6420dd37689806a6754da1a
do-oni <dooniseo@gmail.com>
2022-02-21 23:43:25 +0900

    [Codility] Count Div

---
+++ practice/codility/CountDiv.java
@@ -0,0 +1,30 @@
+package main.java.algorithm.practice.codility;
+
+public class CountDiv {
+
+    // Performance 50
+    public int solution1(int A, int B, int K) {
+        // write your code in Java SE 8
+        int cnt = 0;
+
+        for (int i = A; i <= B; i++) {
+            if (i % K == 0) {
+                cnt++;
+            } else {
+                continue;
+            }
+        }
+        return cnt;
+    }
+
+    // Performance 100
+    public int solution2(int A, int B, int K) {
+        // write your code in Java SE 8
+        int cnt = (B / K) - (A / K);
+
+        if (A % K == 0) cnt += 1;
+
+        return cnt;
+    }
+
+}
