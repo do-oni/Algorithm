134e9fcaa884f7fca15fef16cb3657c209e039ba
do-oni <dooniseo@gmail.com>
2022-02-23 00:07:01 +0900

    [Codility] Triangle

---
+++ practice/codility/Triangle.java
@@ -0,0 +1,23 @@
+package main.java.algorithm.practice.codility;
+
+import java.util.Arrays;
+
+public class Triangle {
+
+    // O(N*log(N))
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        Arrays.sort(A);
+
+        int size = A.length;
+
+        if (size < 3) return 0;
+
+        for (int i = 0; i < size - 2; i++) {
+            long P = A[i], Q = A[i + 1], R = A[i + 2];
+            if (P + Q > R) return 1;
+        }
+        return 0;
+    }
+
+}
