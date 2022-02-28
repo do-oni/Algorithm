a4d6b27b91dabe78dae3d1499f2b47d01c94b90c
do-oni <dooniseo@gmail.com>
2022-02-28 15:20:41 +0900

    [LeetCode] Split A String In Balanced Strings

---
+++ practice/leetcode/greedy/SplitAStringInBalancedStrings.java
@@ -0,0 +1,19 @@
+package main.java.algorithm.practice.leetcode.greedy;
+
+public class SplitAStringInBalancedStrings {
+
+    public int balancedStringSplit(String s) {
+        int cnt = 0;
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'R') cnt += 1;
+            else cnt -= 1;
+
+            if (cnt == 0) answer += 1;
+        }
+
+        return answer;
+    }
+
+}
