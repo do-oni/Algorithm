49ef999e36a49bd26836733284be8ddae91a3846
do-oni <dooniseo@gmail.com>
2022-03-02 13:01:16 +0900

    update

--- practice/leetcode/greedy/MinimumSumOfFourDigitNumberAfterSplittingDigit.java
+++ practice/leetcode/greedy/MinimumSumOfFourDigitNumberAfterSplittingDigit.java
@@ -1,17 +1,20 @@
 package main.java.algorithm.practice.leetcode.greedy;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class MinimumSumOfFourDigitNumberAfterSplittingDigit {
 
-public int minimumSum(int num){
+    public int minimumSum(int num) {
 
-        int[]nums=Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
+        int[] nums = Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
 
-        Arrqays.sort(nums);
+        Arrays.sort(nums);
 
-        int s1=nums[0]*10+nums[3];
-        int s2=nums[1]*10+nums[2];
+        int s1 = nums[0] * 10 + nums[3];
+        int s2 = nums[1] * 10 + nums[2];
 
-        return s1+s2;
-        }
+        return s1 + s2;
+    }
 
-        }
+}
